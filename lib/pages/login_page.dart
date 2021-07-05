62a97ffa4b2e2a9e949034753661792698593c5f
AmnaZafar21 <amna.bscs3921@iiu.edu.pk>
2021-07-06 00:17:04 +0500

    Day 5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,47 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:new_projectcatelog/utilities/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color:Colors.white,
-      child: Column(
-        children: [Image.asset("assets/images/login_image.png",fit:BoxFit.cover,
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          //ctrl+shift+R
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            Text(
+              "Welcome ",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            ElevatedButton(
+              child: Text("Login"),
+              style: TextButton.styleFrom(minimumSize: Size(120, 40)),
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter USER Name",
+                      labelText: "UserName",
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      //obscureText:true,
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-
-        Text("Welcome ",
-        style: TextStyle( fontSize:24,
-        fontWeight:FontWeight.bold),
       ),
-      SizedBox(
-        height: 20.0,
-      ),
-      ElevatedButton( 
-      child: Text("Login"),
-      style: TextButton.styleFrom(),
-      onPressed:  () {},
-      ),
-      Padding(padding: const EdgeInsets.all(16.0),
-    child: Column(children:
-     [ TextFormField(decoration:InputDecoration(
-        hintText: "Enter USER Name",
-        labelText: "UserName",
-      ),
-      ),
-       TextFormField(decoration:InputDecoration(
-         //obscureText:true,
-        hintText: "Enter Password",
-        labelText:"Password",
-      ),
-      ),
-      ],
-      ),
-      ),
-        ],
-     ),
-
-        
-
     );
   }
 }
